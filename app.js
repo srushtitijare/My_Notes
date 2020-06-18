19e4b8bd37b8b673aaaae82c6c3f26cdcc33117e
srushtitijare <tijaresrushti@gmail.com>
2020-06-18 23:44:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+var items = ["Srushti","Anil","Tijare"];
+var workitems = [];
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+  var day = date.getDate();
+
+  res.render("list",{listTitle: day, newListItems: items});
+});
+
+app.post("/",function(req,res){
+  var item = req.body.newItem;
+  console.log(req.body);
+  if(req.body.list === "Work")
+  {
+    workitems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item)
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle: "Work List", newListItems: workitems});
+});
+
+app.get("/about",function(req,res){
+  res.render("about");
+});
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
